4cc2e26d39bd36ed8981524afb71ba0764a28e7b
Julio M Cruz <julio.cruz@eb-ms.net>
2023-09-17 07:25:39 -0500

    Update forms

--- App/components/BridgeToken.tsx
+++ App/components/BridgeToken.tsx
@@ -1,37 +1,69 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { Button } from "./ui/button";
 
 import { useAccount } from "wagmi";
 import type { NextComponentType } from "next";
 
-import WormholeBridge from '@wormhole-foundation/wormhole-connect';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
+
+import WormholeBridge from '@wormhole-foundation/wormhole-connect';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "../components/ui/dialog";
+  
 const BridgeUserToken: NextComponentType = () => {
 
     const { isConnected, address } = useAccount();
     const [ showBridge, setShowBridge ] = useState(false);
     const [ buttonBridge, setButtonBridge ] = useState("Show Bridge");
+    const [isClient, setIsClient] = useState(false);
+    const [openDialog, setOpenDialog] = useState(false);
+
+
+    useEffect(() => {
+        setIsClient(true);
+      }, []);
 
     const toogleBridgeToken = () => {
-        setShowBridge(!showBridge);
-        if (showBridge) {
-            setShowBridge(false);
-            setButtonBridge("Open Bridge")
-        } else {
-            setShowBridge(true);
-            setButtonBridge("Close Bridge")
-        }
+        setOpenDialog(true);
     };
 
     return (
         <>
         <div className="flex items-center">
-        <Button variant="outline" onClick={() => toogleBridgeToken() }>{buttonBridge}</Button>
-            { (showBridge) && (
-                <WormholeBridge />
-            )
-            }
-        </div>
+
+            {isClient && !isConnected && <ConnectButton />}
+            {isClient && isConnected && (
+              <>
+                <Button onClick={() => toogleBridgeToken()} variant="outline">Bridge Tokens</Button>
+
+                <Dialog open={openDialog} onOpenChange={setOpenDialog} >
+                  <DialogContent className="sm:max-w-[425px] w-[600px]">
+
+                    <div className="flex flex-col items-center justify-center w-96">
+                        <WormholeBridge  />
+                    </div>
+
+                    <DialogFooter>
+                      <Button
+                        variant={"outline"}
+                        onClick={() => setOpenDialog(false)}
+                      >
+                        Close
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </>
+            )}
+                 </div> 
         </>
     )
 }
